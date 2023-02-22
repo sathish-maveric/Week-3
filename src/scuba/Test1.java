bf8c4d103e2aa2ddc21bb3638c8347bc703a7870
Laxman211 <laxmanb@maveric-systems.com>
2023-02-22 23:50:29 +0530

    first commit

---
+++ src/scuba/Test1.java
@@ -0,0 +1,36 @@
+package scuba;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+		
+public class Test1 {
+
+	public static void main(String[] args) {
+
+				//Navigating the selenium code chrome driver Location
+				System.setProperty("webdriver.chrome.driver","C:\\Users\\laxmanb\\Desktop\\Selenium\\Software\\Driver\\chromedriver.exe");
+				WebDriver driver = new ChromeDriver();
+				
+				
+				//Launching the Web site 
+				driver.get("https://www.saucedemo.com/");
+				
+				//Locating and entering values
+				By uName = By.id("user-name");
+				driver.findElement(uName).sendKeys("problem_user");
+				
+				By pWord = By.id("password");
+				driver.findElement(pWord).sendKeys("secret_sauce");
+				
+				By loginBtn = By.id("login-button");
+				driver.findElement(loginBtn).click();
+				
+				//driver.quit();
+				
+			}
+
+	}
+
+
